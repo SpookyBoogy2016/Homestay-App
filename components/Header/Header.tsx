50e082e667485600ea23ed1049b2a741c3016d6b
Tino Liu <justinoliu.work@gmail.com>
2023-02-05 11:30:50 +0800

    Feature: added date range picker

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/legacy/image";
 import {
   MagnifyingGlassIcon,
   GlobeAltIcon,
   Bars3Icon,
   UserCircleIcon,
+  UsersIcon,
 } from "@heroicons/react/20/solid";
+import { DateRangePicker } from "react-date-range";
+import { addDays } from "date-fns";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css";
+import { DateRangePickerProps } from "react-date-range";
+
+// TODO: Figure out why this interface isn't working
+// type Selection = {
+//   selection: { endDate: Date; startDate: Date; key: string };
+// };
 
 function Header() {
+  const [searchInput, setSearchInput] = useState("");
+  const [guestNum, setGuestNum] = useState(1);
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: addDays(new Date(), 2),
+      key: "selection",
+    },
+  ]);
+
+  console.log("Tino log test ---> WHAT ", date);
+
   return (
     <header className="sticky z-50 top-0 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* Left - Logo */}
@@ -23,6 +46,10 @@ function Header() {
       {/* Center - Search */}
       <div className="flex items-center md:border-2 md:shadow-md py-2 px-5 rounded-xl">
         <input
+          value={searchInput}
+          onChange={(e: any) => {
+            setSearchInput(e.target.value);
+          }}
           type="text"
           placeholder="Start your Search"
           className="flex-grow bg-transparent outline-none text-gray-500 text-sm placeholder-gray-400"
@@ -38,6 +65,37 @@ function Header() {
           <UserCircleIcon className="h-6" />
         </div>
       </div>
+
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto mt-3">
+          <DateRangePicker
+            ranges={date}
+            minDate={new Date()}
+            // TODO: DateRangePIckerProps doesn't have item.selection... need to check
+            onChange={(item: DateRangePickerProps | any) =>
+              setDate([item.selection])
+            }
+            rangeColors={["#FD5B61"]}
+          />
+          <div className="flex border-b items-center">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              type="number"
+              value={guestNum}
+              min={1}
+              onChange={(e: any) => setGuestNum(e.target.value)}
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+            />
+          </div>
+          <div>
+            <button></button>
+            <button></button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
--- public/types/typings.Home.ts
+++ public/types/typings.Home.ts
@@ -2,3 +2,7 @@ export interface Props {
   exploreData: [img: string, location: string, distance: string];
   cardsData: [img: string, title: string];
 }
+
+export interface img {
+  img: string;
+}
